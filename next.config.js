9e193e179b0011f2d53abe3269af6b7f66fb9aa3
Automaton <emmanuelyanum@gmail.com>
2021-06-15 15:40:14 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        DB_LOCAL_URI: '',
+        DB_URI: '',
+
+
+
+        CLOUDINARY_CLOUD_NAME: '',
+        CLOUDINARY_API_KEY: '',
+        CLOUDINARY_API_SECRET: '',
+
+
+        SMTP_HOST: "",
+        SMTP_PORT: "",
+        SMTP_USER: "",
+        SMTP_PASSWORD: "",
+        SMTP_FROM_EMAIL: "",
+        SMTP_FROM_NAME: "",
+
+
+        NEXTAUTH_URL: '',
+    },
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+}
